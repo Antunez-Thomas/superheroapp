c7d134882531e4c96ad62f5a8d24a89d862854c8
Thom <thomas.antunez0@gmail.com>
2025-12-16 10:49:44 -0300

    Hero Item check

--- app/src/main/java/com/example/superheroapp/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroapp/HeroesScreen.kt
@@ -1,10 +1,11 @@
 package com.example.superheroapp
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
@@ -13,11 +14,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroapp.model.Hero
+import com.example.superheroapp.ui.theme.SuperHeroAppTheme
 
 @Composable
 fun HeroItem(
@@ -29,45 +33,50 @@ fun HeroItem(
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         shape = MaterialTheme.shapes.medium,
         modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp)
         ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Row (
-                modifier = Modifier.fillMaxWidth(),
+            Column(
+                modifier = Modifier
+                    .height(72.dp)
             ) {
-                Column(modifier = Modifier.weight(2f)) {
-                    Text(
-                        text = stringResource(hero.nameRes),
-                        style = MaterialTheme.typography.displaySmall
-                    )
-                    Text(
-                        text = stringResource(hero.descriptionRes),
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                }
-                Image(
-                    painter = painterResource(hero.imageRes),
-                    contentDescription = stringResource(hero.nameRes),
-                    modifier = Modifier
-                        .size(72.dp)
-                        .weight(1f)
-                        .padding(start = 16.dp)
+                Text(
+                    text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall
+                )
+                Text(
+                    text = stringResource(hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge
                 )
             }
+            Spacer(modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+            )
+            Image(
+                painter = painterResource(hero.imageRes),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(MaterialTheme.shapes.small),
+            )
         }
     }
 }
+
 @Preview
 @Composable
 fun HeroItemPreview() {
-    HeroItem(
-        hero = Hero(
-            R.string.hero1,
-            R.string.description1,
-            R.drawable.android_superhero1
+    SuperHeroAppTheme {
+        HeroItem(
+            hero = Hero(
+                R.string.hero1,
+                R.string.description1,
+                R.drawable.android_superhero1
+            )
         )
-    )
+    }
 }
