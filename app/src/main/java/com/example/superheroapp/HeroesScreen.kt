1672f3eb47d0216fbb6f4c54a78411950ecf04b1
Thom <thomas.antunez0@gmail.com>
2025-12-16 01:03:51 -0300

    HeroItem more completed

--- app/src/main/java/com/example/superheroapp/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroapp/HeroesScreen.kt
@@ -1,8 +1,12 @@
 package com.example.superheroapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroapp.model.Hero
 
@@ -21,18 +26,49 @@ fun HeroItem(
 ) {
     Card(
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
-        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        shape = MaterialTheme.shapes.medium,
+        modifier = modifier
         ) {
-        Row() {
-            Column() {
-                Text(
-                    text = stringResource(hero.nameRes)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+
+            ) {
+                Column(modifier = Modifier.weight(2f)) {
+                    Text(
+                        text = stringResource(hero.nameRes),
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                    Text(
+                        text = stringResource(hero.descriptionRes),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+                Image(
+                    painter = painterResource(hero.imageRes),
+                    contentDescription = stringResource(hero.nameRes),
+                    modifier = Modifier
+                        .size(72.dp)
+                        .weight(1f)
+                        .padding(start = 16.dp)
                 )
             }
-            Image(
-                painter = painterResource(hero.imageRes),
-                contentDescription = stringResource(hero.nameRes)
-            )
         }
     }
+}
+@Preview
+@Composable
+fun HeroItemPreview() {
+    HeroItem(
+        hero = Hero(
+            R.string.hero1,
+            R.string.description1,
+            R.drawable.android_superhero1
+        )
+    )
 }
